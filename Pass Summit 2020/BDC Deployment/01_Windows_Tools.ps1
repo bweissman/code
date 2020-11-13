104bd12ba6bc286d4b261423c1d9aa9074cb27e7
Ben Weissman <b.weissman@solisyon.de>
2020-11-13 13:10:58 +0100

    Update 01_Windows_Tools.ps1

--- Pass Summit 2020/BDC Deployment/01_Windows_Tools.ps1
+++ Pass Summit 2020/BDC Deployment/01_Windows_Tools.ps1
@@ -4,6 +4,10 @@ choco install azure-data-studio -y
 choco install azure-cli -y
 choco install kubernetes-cli -y
 choco install curl -y
+choco install putty -y
+choco install notepadplusplus -y
+choco install 7zip -y
+choco install sqlserver-cmdlineutils -y
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 curl -o azdata.msi https://aka.ms/azdata-msi
 msiexec /i azdata.msi /passive
