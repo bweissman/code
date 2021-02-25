5287307124b60f0167aa2db3fcf5f329e91e5caa
Ben Weissman <b.weissman@solisyon.de>
2021-02-25 10:23:28 +0100

    initial commit

---
+++ MVP/MVPCheck.ps1
@@ -0,0 +1,23 @@
+﻿# Install-module -Name MVP -Scope CurrentUser -Repository PSGallery
+# More Details on the module and how to get a subscription key: https://github.com/lazywinadmin/MVP
+Import-Module MVP
+$SubscriptionKey = ''
+$StartDate = "2020-04-01"
+$MVPName="Weissman"
+Set-MVPConfiguration -SubscriptionKey $SubscriptionKey
+$Contributions=Get-MVPContribution -Limit 1000 | Select StartDate,Title,ReferenceUrl,Description | Where  {$_.ReferenceUrl -like "http*"} | where {$_.StartDate -gt $StartDate } 
+foreach ($cont in $Contributions) {
+$ContainsName = 0
+try   { 
+        $res=curl($cont.ReferenceUrl) 
+        $cont.Description = "Name missing"
+        if ($cont.ReferenceUrl -like "*.sessionize.com*") { $cont.Description = "Name missing (Sessionize)" }
+        if ($cont.ReferenceUrl -like "*.pdf") { $cont.Description = "Name missing (PDF)" }
+        if ($res -like "*Looking for PASS or SQLSaturday?*") { $cont.Description = "URL Invalid" }
+        if ($res -like "*$MVPName*") { $cont.Description = "OK" }}
+catch { 
+        $res="" 
+        $cont.Description = "URL Invalid" 
+        }
+}
+$Contributions | Where {$_.Description -ne "OK"} | Sort-Object Description, StartDate | Format-Table StartDate,Title,ReferenceUrl,Description 
---
+++ MVP/README.md
@@ -0,0 +1,11 @@
+# MVP Check
+Get a subscription ID (see https://github.com/lazywinadmin/MVP) and set it using the SubscriptionKey variable
+
+Also adjust the name to look for and the StartDate
+
+The script will check all your contributions that have a URL, if:
+
+- The URL is valid
+- If your name is on that page (doesn't work for sessionize, PDFs etc.)
+
+Hope, this is helpful!
