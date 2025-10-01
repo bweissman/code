8453e27f3c3d34b2e0d3b3407dbe03caf16073c7
Heini Ilmarinen <heini.ilmarinen@live.com>
2025-10-01 21:24:36 +0300

    Adding Heini's demos for precon

---
+++ Precon2025/m02/03 - Fabric Data Engineering.ps1
@@ -0,0 +1,8 @@
+Start-Process https://app.fabric.microsoft.com/
+
+# Create a lakehouse
+# File upload
+# Creating and managing tables
+# Explore data in Lakehouse
+# Using notebooks
+# Check Spark cluster setup
---
+++ Precon2025/m06/01 - ETL in Fabric.ps1
@@ -0,0 +1,7 @@
+Start-Process https://app.fabric.microsoft.com/
+
+# Setting up a vnet GW
+# Create Pipeline with vnet gateway connection
+# Create Notebook for with transformations
+# Create T-SQL transformations
+# Add all steps in the pipeline
